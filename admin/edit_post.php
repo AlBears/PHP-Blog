c8f1eba0afce85d8bd4d75e8210a5f9487b3ca00
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-11 12:13:18 +0300

    Fetching DB data to admin

--- admin/edit_post.php
+++ admin/edit_post.php
@@ -1,28 +1,53 @@
 <?php include 'includes/header.php'; ?>
+<?php
+
+$id = $_GET['id'];
+
+$db = new Database();
+
+//Create query
+$query = "SELECT * FROM posts WHERE id = ".$id;
+//Use query
+$post = $db->select($query)->fetch_assoc();
+
+//Create query
+$query = "SELECT * FROM categories";
+//Use query
+$categories = $db->select($query);
+
+?>
 
 <form role="form" method="post" action="edit_post.php">
   <div class="form-group">
     <label>Post Title</label>
-    <input name="title" type="text" class="form-control" placeholder="Enter Title">
+    <input value="<?php echo $post['title'];?>"name="title" type="text" class="form-control" placeholder="Enter Title">
   </div>
   <div class="form-group">
     <label>Post Body</label>
-    <textarea name="body" class="form-control" placeholder="Enter Post Body"></textarea>
+    <textarea  name="body" class="form-control" placeholder="Enter Post Body"><?php echo $post['body'];?></textarea>
   </div>
   <div class="form-group">
     <label>Category</label>
+    
     <select name="category" class="form-control">
-		<option>News</option>
-		<option>Events</option>
+  <?php while($row = $categories->fetch_assoc()) :?>
+  <?php if($row['id'] == $post['category']) { 
+    $selected = 'selected';
+    } else { 
+     $selected = '';}
+     ?>
+    <option <?php echo $selected; ?>><?php echo $row['name']; ?></option>
+    <?php endwhile; ?>
 	</select>
+   
   </div>
   <div class="form-group">
     <label>Author</label>
-    <input name="author" type="text" class="form-control" placeholder="Enter Author Name">
+    <input value="<?php echo $post['author'];?>" name="author" type="text" class="form-control" placeholder="Enter Author Name">
   </div>
   <div class="form-group">
     <label>Tags</label>
-    <input name="tags" type="text" class="form-control" placeholder="Enter Tags">
+    <input value="<?php echo $post['tags'];?>"name="tags" type="text" class="form-control" placeholder="Enter Tags">
   </div>
   <div>
 	<input name="submit" type="submit" class="btn btn-default" value="Submit" />
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php'; ?>
+<?php
+$db = new Database;
+
+$query = "SELECT posts.*, categories.name FROM posts
+		INNER JOIN categories
+		ON posts.category = categories.id";
+
+$posts = $db->select($query);
+
+$query = "SELECT * FROM categories";
+
+$categories = $db->select($query);
+
+?>
 
 <table class="table table-striped">
 	<tr>
@@ -8,13 +22,15 @@
 		<th>Author</th>
 		<th>Date</th>
 	</tr>
+<?php while($row = $posts->fetch_assoc()) : ?>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td><?php echo $row['id']; ?></td>
+		<td><a href='edit_post.php?id=<?php echo $row['id']; ?>'><?php echo $row['title']; ?></td>
+		<td><?php echo $row['name']; ?></td>
+		<td><?php echo $row['author']; ?></td>
+		<td><?php echo formatDate($row['date']); ?></td>
 	</tr>
+	<?php endwhile; ?>
 </table>
 
 
@@ -23,10 +39,12 @@
 		<th>Category ID#</th>
 		<th>Category Name</th>
 	</tr>
+	<?php while($row = $categories->fetch_assoc()) : ?>
 	<tr>
-		<td></td>
-		<td></td>
+		<td><?php echo $row['id']; ?></td>
+		<td><a href='edit_category.php?id=<?php echo $row['id']; ?>'><?php echo $row['name']; ?></td>
 	</tr>
+	<?php endwhile; ?>
 </table>
 
 
